c11becb5b93d96abde865d78c25a0a93e4c45119
Chris Hibbert <hibbert@agoric.com>
2020-12-28 17:35:10 -0800

    test: update loan test to use a Notifier
    see-also: https://github.com/Agoric/agoric-sdk/pull/2144

--- snippets/zoe/contracts/test-loan.js
+++ snippets/zoe/contracts/test-loan.js
@@ -76,6 +76,7 @@ test('loan contract', async t => {
     priceAuthority,
     periodNotifier,
     interestRate: 5,
+    interestPeriod: 5,
   };
 
   const { creatorInvitation: lendInvitation } = await E(zoe).startInstance(
